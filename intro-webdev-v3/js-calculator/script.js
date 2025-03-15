1f0e649211ed4c7c61a37df1c3633812cc1496da
Andrew Huntington <andrew.huntington@gmail.com>
2025-03-15 19:09:56 +0900

    begin implementing javascript

--- intro-webdev-v3/js-calculator/script.js
+++ intro-webdev-v3/js-calculator/script.js
@@ -1,1 +1,17 @@
+// number to display to the user
+let displayNumber = 0;
+// background total; may differ from what is currently displayed
+let total = 0;
 
+const display = document.querySelector(".number-display");
+display.innerHTML = displayNumber;
+
+const calc = document.querySelector(".calculator");
+
+Array.from(calc.children).forEach((e) => {
+  if (e.className === "display") return;
+
+  e.addEventListener("click", (e) => {
+    console.log(e.target.className.split(" ")[0]);
+  });
+});
